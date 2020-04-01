f793de86919d1ff6a4cab77c71af44918383a115
hanhnq <hanhnq@feelsyncsystem.vn>
2020-04-01 09:59:10 +0700

    Compiling Assets

--- public/js/app.js
+++ public/js/app.js
@@ -37117,15 +37117,51 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/custom.scss":
+/*!************************************!*\
+  !*** ./resources/sass/custom.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/login.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/login.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/style.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/style.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!**************************************************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ./resources/sass/custom.scss ./resources/sass/login.scss ***!
+  \**************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! D:\xampp7430\htdocs\fss_projects\thesismanagement\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\xampp7430\htdocs\fss_projects\thesismanagement\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\xampp7430\htdocs\fss_projects\thesismanagement\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\xampp7430\htdocs\fss_projects\thesismanagement\resources\sass\style.scss */"./resources/sass/style.scss");
+__webpack_require__(/*! D:\xampp7430\htdocs\fss_projects\thesismanagement\resources\sass\custom.scss */"./resources/sass/custom.scss");
+module.exports = __webpack_require__(/*! D:\xampp7430\htdocs\fss_projects\thesismanagement\resources\sass\login.scss */"./resources/sass/login.scss");
 
 
 /***/ })
